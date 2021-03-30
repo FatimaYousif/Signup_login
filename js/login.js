b749a45c33b3b417f18c79a45f521a6000016071
msaaddev <mrsaadirfan@gmail.com>
2021-03-31 01:08:12 +0500

    👌 IMPROVE: formatting

--- js/login.js
+++ js/login.js
@@ -1,25 +1,18 @@
-var signin=document.getElementById("signin");
+var signin = document.getElementById('signin');
 
+signin.addEventListener('click', function () {
+    var ae = localStorage.getItem('email');
+    var ap = localStorage.getItem('pass');
+    var email = document.getElementById('email').value;
+    var pw = document.getElementById('pw').value;
+    var show = document.getElementById('show');
 
-signin.addEventListener("click", function()
-{
-    var ae=localStorage.getItem("email");
-    var ap=localStorage.getItem("pass");
-    var email=document.getElementById("email").value;
-    var pw=document.getElementById("pw").value;
-    var show=document.getElementById("show");
-    
-
-    if(ae === email && ap===pw)
-    {
-        //change the location of it by connecting to another page 
-        // by 
+    if (ae === email && ap === pw) {
+        //change the location of it by connecting to another page
+        // by
         // window.location.href= "otherpage.html"
-        alert("login successful");
+        alert('login successful');
+    } else {
+        show.style.display = 'block';
     }
-    else{   
-        show.style.display="block";
-    
-    }
-})
-
+});
